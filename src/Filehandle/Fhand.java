05f548403c603f7676ddc58baef3629bea7efea9
Abhiram P <abhiram.p@ecstvm.local>
2020-09-21 13:16:28 +0530

    FileHandling

---
+++ src/Filehandle/Fhand.java
@@ -0,0 +1,17 @@
+package Filehandle;
+
+import java.io.*;
+
+public class Fhand {
+    public static void main(String[] args) throws Exception {
+        File file=new File("home.txt");
+        FileOutputStream fos=new FileOutputStream(file);
+        DataOutputStream dos=new DataOutputStream(fos);
+        dos.writeUTF("Abhiram");
+
+        FileInputStream fis=new FileInputStream(file);
+        DataInputStream din=new DataInputStream(fis);
+        String str=din.readUTF();
+        System.out.println(str);
+    }
+}
