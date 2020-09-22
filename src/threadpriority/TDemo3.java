69c41e997700989635b430611e5c75aa149c7641
Abhiram P <abhiram.p@ecstvm.local>
2020-09-23 02:38:32 +0530

    Upload

---
+++ src/threadpriority/TDemo3.java
@@ -0,0 +1,41 @@
+package threadpriority;
+
+public class TDemo3 {
+    public static void main(String[] args) throws InterruptedException {
+        Thread t1= new Thread(()->{
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Hello");
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        },"Hi Thd");
+        Thread t2=new Thread(()-> {
+            for (int i = 0; i < 5; i++) {
+                System.out.println("Hi");
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        },"Hello THD");
+        System.out.println(t1.getName());
+        t1.setPriority(Thread.MIN_PRIORITY);
+        System.out.println(t1.getPriority());
+        t2.setPriority(Thread.MAX_PRIORITY);
+        System.out.println(t2.getName());
+        System.out.println(t2.getPriority());
+        t1.start();
+        Thread.sleep(10);
+        t2.start();
+
+        t1.join();// main wait till t1 complete it's execution
+        t2.join();// main wait till t1 complete it's execution
+
+        System.out.println(t1.isAlive());
+        System.out.println("Bye");
+    }
+}
